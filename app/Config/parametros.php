a03b307780fcc3410ada9999c957e98696f6a6c6
Javier Cebrián Muñoz <a20cemuja@iesgrancapitan.org>
2022-01-24 13:52:22 +0100

    feat: enrutamiento dinamico  - ahora tenemos una clase en app/Core/Router.php que almacena un array con las acciones con su direccion  - index.php almacena todas las rutas en el Router y lo llama para comprobar cual debe tomar  - ! añado el app/Config/configuraciones.php de forma didactica. Estoy compartiendo las credenciales de la BD

---
+++ app/Config/parametros.php
@@ -0,0 +1,12 @@
+<?php
+
+define('HOST', 'localhost');
+define('USER', 'phpmyadmin');
+define('PASS', 'admin');
+define('BD_NAME', 'superheroes');
+define('PORT', '3306');
+
+define('DIRBASEURL', 'proyecto/public/index.php');
+define('DIRPUBLIC', 'proyecto/public');
+
+define('SHPORPAGINA', 5);
--- public/index.php
+++ public/index.php
@@ -1,17 +1,37 @@
 <?php
 //Front Controller
+require '../vendor/autoload.php';
+require '../app/Config/parametros.php';
+
+use App\Controller\DefaultController;
+use App\Core\Router;
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require '../vendor/autoload.php';
+$router = new Router();
 
-use App\Controller\DefaultController;
+$router->add(array(
+    'name' => 'holaMundo',
+    'path' => '/holamundo$/',
+    'action' => [DefaultController::class, 'saludaAction']
+));
 
-$controller = new DefaultController();
+$router->add(array(
+    'name' => 'numeros',
+    'path' => '/numeros$/',
+    'action' => [DefaultController::class, 'numerosAction']
+));
 
-if ($_SERVER['PATH_INFO'] == '/saludo')
-    $controller->saludaAction();
-else if ($_SERVER['PATH_INFO'] == '/numeros')
-    $controller->numerosAction();
+$request = str_replace(DIRBASEURL, '', $_SERVER['REQUEST_URI']);
+$route = $router->match($request);
+
+if ($route) {
+    $controllerName = $route['action'][0];
+    $actionName = $route['action'][1];
+    $controller = new $controllerName;
+    $controller->$actionName($request);
+} else {
+    echo "no route";
+}
